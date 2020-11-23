f3399c636d987fd718ad53d532b633ab4a12324f
yuta0801 <yuta030801@gmail.com>
2020-11-23 18:45:19 +0900

    convert input and output with Uint8Array

--- mod.js
+++ mod.js
@@ -21,6 +21,25 @@ const pluginPath = join(
 const pluginId = Deno.openPlugin(pluginPath);
 const { renderOp: renderOpId } = Deno.core.ops();
 
-export function render(input, buffer) {
-  return Deno.core.dispatch(renderOpId, input, buffer);
+export function render(svg, buffer = new Uint8Array(64 * 1024)) {
+  const input = convertStringToArrayBuffer(svg);
+  const res = Deno.core.dispatch(renderOpId, input, buffer);
+  if (!res) throw new Error("Failed to render svg");
+  const size = convertU64BytesToNumber(res);
+  return buffer.slice(0, size);
+}
+
+function convertStringToArrayBuffer(svg) {
+  if (typeof svg !== "string") return svg;
+  return new TextEncoder().encode(svg);
+}
+
+function convertU64BytesToNumber(bytes) {
+  const arr = new Uint8Array(bytes);
+  const view = new DataView(arr.buffer);
+  const bigint = view.getBigUint64(0);
+  if (bigint > Number.MAX_SAFE_INTEGER) {
+    throw new Error(`Cannot represent ${bigint} as Number safely`);
+  }
+  return Number(bigint);
 }
